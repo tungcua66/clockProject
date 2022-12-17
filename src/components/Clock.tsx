2e7b8b4e11e346c5120388c9f910f47424fd71ef
tung <tung.tran@oppus.fr>
2022-12-17 14:46:29 +0100

    adding logic to increase hour or minute

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -30,6 +30,14 @@ const TimeContainer = styled.div(({ lightModeIsOn = false }) => ({
   fontFamily: 'Orbitron',
   fontSize: '2em',
 }));
+const TimeUnit = styled.div(({ lightModeIsOn = false, isSelected = false }) => ({
+  width: '1em',
+  height: '1em',
+  color: lightModeIsOn ? 'green' : 'black',
+  fontFamily: 'Orbitron',
+  fontSize: '2em',
+  backgroundColor: isSelected ? '#e7f0a5' : 'none',
+}));
 const ButtonContainer = styled.button(({ justifySelf }) => ({
   width: '50px',
   height: '30px',
@@ -40,23 +48,41 @@ const ButtonContainer = styled.button(({ justifySelf }) => ({
 const Clock = () => {
   const [date, setDate] = useState(new Date());
   const [lightModeIsOn, setLightMode] = useState(false);
+  const [mode, setMode] = useState(0); // 0 = noEditable, 1 = hour, 2 = minutes
+  const [timeAcc, setTimeAcc] = useState({ hourAcc: 0, minuteAcc: 0 });
 
   const lightModeOnClickHandler = () => {
-    // e.preventDefault();
-    console.log('hey im in');
     setLightMode((prev) => !prev);
   };
 
-  const refreshClock = () => {
-    setDate(new Date());
+  const buttonModeOnClickHandler = () => {
+    setMode((prev) => {
+      if (prev === 2) {
+        return 0;
+      }
+      return prev + 1;
+    });
+  };
+
+  const buttonIncreaseOnClickHandler = () => {
+    if (mode === 1) {
+      setTimeAcc({ ...timeAcc, hourAcc: timeAcc.hourAcc + 1 });
+    }
+    if (mode === 2) {
+      setTimeAcc({ ...timeAcc, minuteAcc: timeAcc.minuteAcc + 1 });
+    }
   };
 
   useEffect(() => {
-    const timerId = setInterval(refreshClock, 1000);
+    const timerId = setInterval(() => {
+      const newDate = new Date();
+      newDate.setHours(newDate.getHours() + timeAcc.hourAcc);
+      newDate.setMinutes(newDate.getMinutes() + timeAcc.minuteAcc);
+      setDate(newDate);
+    }, 100);
     return () => { clearInterval(timerId); };
-  }, []);
+  }, [timeAcc]);
 
-  // return <div>{date.toLocaleTimeString()}</div>;
   return (
     <MainContainer className="MainContainer">
       <BlockContainer className="LeftBlock">
@@ -66,12 +92,42 @@ const Clock = () => {
         />
       </BlockContainer>
       <TimeContainer lightModeIsOn={lightModeIsOn}>
-        {' '}
-        {date.toLocaleTimeString()}
+        <TimeUnit
+          className="Hour"
+          lightModeIsOn={lightModeIsOn}
+          isSelected={mode === 1}
+        >
+          {' '}
+          {date.getHours() < 10 ? `0${date.getHours()}` : date.getHours()}
+
+        </TimeUnit>
+        :
+        <TimeUnit
+          className="Minutes"
+          lightModeIsOn={lightModeIsOn}
+          isSelected={mode === 2}
+
+        >
+          {' '}
+          {date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes()}
+        </TimeUnit>
+        :
+        <TimeUnit className="Seconds" lightModeIsOn={lightModeIsOn}>
+          {' '}
+          {date.getSeconds() < 10 ? `0${date.getSeconds()}` : date.getSeconds()}
+        </TimeUnit>
       </TimeContainer>
       <BlockContainer className="RightBlock">
-        <ButtonContainer className="ButtonMode" date={date} />
-        <ButtonContainer className="ButtonIncrease" justifySelf="start" />
+        <ButtonContainer
+          className="ButtonMode"
+          date={date}
+          onMouseDown={() => { buttonModeOnClickHandler(); }}
+        />
+        <ButtonContainer
+          className="ButtonIncrease"
+          justifySelf="start"
+          onMouseDown={() => { buttonIncreaseOnClickHandler(); }}
+        />
       </BlockContainer>
 
     </MainContainer>
